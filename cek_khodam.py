6af23f5b7d40cac44cf2b62225cc47f499d6d840
FaldoM-student <faldomakay09@gmail.com>
2025-11-25 20:24:17 +0800

    Update cek_khodam.py
    Menambahkan format motivasi dan menghapus baris kode yang tidak diperlukan

--- cek_khodam.py
+++ cek_khodam.py
@@ -3,9 +3,15 @@ import random
 def cek_khodam(nama):
     daftar_khodam = []
 
+    motivasi = {}
+
     khodam = random.choice(daftar_khodam)
-    return f"Khodam yang menemani {nama}: {khodam}"
+    pesan = motivasi[khodam]
+    
+    return (
+        f"Khodam yang menemani {nama}: {khodam}"
+        f"Pesan Khodam: {pesan}
+    )
 
 nama = input("Masukkan nama kamu: ")
-umur = input("Masukkan umur kamu: ")
 print(cek_khodam(nama))
